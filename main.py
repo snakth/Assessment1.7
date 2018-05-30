e94ccbde5d7ce84dfe561ee0ad3df584e3a160c3
Max Zuev <snakth@live.ru>
2018-05-30 23:40:42 +0300

    Third Commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Animals:
     head = 1
     body = 1
@@ -5,6 +8,7 @@ class Animals:
     wings = 2
 
     coordinates = 0
+
     def __init__(self, coordinates):
         self.coordinates = coordinates
 
@@ -13,30 +17,39 @@ class Mammals(Animals):
     wings = 0
     horns = "yes"
 
-class Cow(Mammals):
+
+class Cow (Mammals):
     sound = "mooo"
+
 
 class Goat(Mammals):
     sound = "baa"
 
+
 class Sheep(Mammals):
     sound = "baa"
+
 
 class Pig(Mammals):
     horns = "no"
     sound = "oink"
 
+
 class Birds(Animals):
     legs = 2
+
 
 class Duck(Birds):
     sound = "quack"
 
+
 class Chicken(Birds):
     sound = "cluck"
 
+
 class Goose(Birds):
     sound = "cackle"
+
 
 cow_0 = Cow(2)
 goat_0 = Goat(5)
@@ -46,4 +59,3 @@ pig_0 = Pig(4)
 duck_0 = Duck(15)
 chicken_0 = Chicken(10)
 goose_0 = Goose(13)
-
